07916fee8d26e661652e8e87b046c22ad6583b0e
Eduardo Russo <russoedu@gmail.com>
2017-01-14 11:30:53 -0200

    [master] First commit

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # elapsy
-Elapsy calculates the elapsed time from the moment the first instance (singleton) is created
+Elapsy is a singleton class that calculates the elapsed time from the moment the first instance until the moment one of the possible functions is called.
+
+Elapsed ir writen in ECMA 6.
+
+## Install
+
+```sh
+npm install --save elapsy
+```
+
+## Usage
+
+In order to use elapsy, you need to instantiate the class when you want to start counting the time. EG right after you start your application
+
+```javascript
+const Elapsy = require('elapsy');
+
+// This will mark the current time for the next calls
+const elapsy = new Elapsy();
+```
+
+When you want to check the execution time, you can use one of the following functions:
+
+### time
+
+Returns the number of the execution time in miliseconds
+
+```javascript
+const elapsy = new Elapsy();
+
+const elapsed = elapsy.time();
+console.log(elapsed);
+```
+
+### formated
+
+Returns a formated string of the execution time in the format hh:mm:ss:ms.
+
+If the elapsed time is more than a day, the number of days is show in the start of the string.
+
+Examples:
+
+00:13:08.324
+
+03:00:59.999
+
+1 day, 12:43:17.385
+
+428 days, 04:58:23.032
+
+```javascript
+const elapsy = new Elapsy();
+
+const formatedElapsed = elapsy.formated();
+console.log(formatedElapsed);
+```
+
+### log
+
+Insert the formated (hh:mm:ss.ms) string of the current elapsed time in the right end of the terminal.
+
+You can pass a [chalk](https://www.npmjs.com/package/chalk) color to the method, and the default is blue;
+
+Example:
+![](https://i.imgur.com/e6uhlB1.png)
+
+```javascript
+const elapsy = new Elapsy();
+
+elapsy.log();
+```
+
---
+++ elapsy.js
@@ -0,0 +1,103 @@
+const chalk = require('chalk');
+
+let instance = null;
+function pad(num, size) {
+  let s = num.toString();
+  while (s.length < size) {
+    s = `0${s}`;
+  }
+  return s;
+}
+
+/**
+ * Singleton class that calculates the elapsed time from the first time a new object wa created until the call
+ * of one of the possible functions
+ * @class ElapsedTime
+ * @module ElapsedTime
+ */
+class Elapsy {
+  /**
+   * Create the singleton instance and set the current time
+   * @constructor
+   * @return {Object} The singleton instance
+   */
+  constructor() {
+    if (!instance) {
+      instance = this;
+    }
+    this.startTime = new Date();
+
+    return instance;
+  }
+
+  /**
+   * Get the current elapsed time in miliseconds
+   * @method elapsedTime
+   * @return {Number} The elapsed time in miliseconds
+   */
+  time(now = new Date().getTime()) {
+    return now - this.startTime.getTime();
+  }
+
+  /**
+   * Get a formated (hh:mm:ss.ms) string of the current elapsed time.
+   * If the elapsed time is more than a day, the number of days is show in the start
+   * of the string
+   * @method formatedElapsedTime
+   * @return {String} The formated elapsed time in miliseconds
+   * @example 00:13:08.324
+   *          03:00:59.999
+   *          1 day, 12:43:17.385
+   *          428 days, 04:58:23.032
+   */
+  formated() {
+    const elapsedTime = this.elapsedTime();
+    let response = '';
+    if (elapsedTime < 1000) {
+      response = `00:00:00.${pad(elapsedTime, 3)}`;
+    } else if (elapsedTime >= 1000 && elapsedTime < 60 * 1000) {
+      const sec = pad(parseInt((elapsedTime / 1000).toString(), 10), 2);
+      const ms = pad(elapsedTime % 1000, 3);
+      response = `00:00:${sec}.${ms}`;
+    } else if (elapsedTime >= 60 * 100 && elapsedTime < 60 * 60 * 100) {
+      const min = pad(parseInt((elapsedTime / 60 / 1000).toString(), 10), 2);
+      const sec = pad(parseInt((elapsedTime / 1000).toString(), 10), 2);
+      const ms = pad(elapsedTime % 1000, 3);
+      response = `00:${min}:${sec}.${ms}`;
+    } else if (elapsedTime >= 60 * 60 * 100 && elapsedTime < 24 * 60 * 60 * 100) {
+      const hour = pad(parseInt((elapsedTime / 60 / 60 / 1000).toString(), 10), 2);
+      const min = pad(parseInt((elapsedTime / 60 / 1000).toString(), 10), 2);
+      const sec = pad(parseInt((elapsedTime / 1000).toString(), 10), 2);
+      const ms = pad(elapsedTime % 1000, 3);
+      response = `${hour}:${min}:${sec}.${ms}`;
+    } else {
+      const day = parseInt((elapsedTime / 24 / 60 / 60 / 1000).toString(), 10);
+      const plural = day === 1 ? '' : 's';
+      const hour = pad(parseInt((elapsedTime / 60 / 60 / 1000).toString(), 10), 2);
+      const min = pad(parseInt((elapsedTime / 60 / 1000).toString(), 10), 2);
+      const sec = pad(parseInt((elapsedTime / 1000).toString(), 10), 2);
+      const ms = pad(elapsedTime % 1000, 3);
+      response = `${day} day${plural}, ${hour}:${min}:${sec}.${ms}`;
+    }
+    return response;
+  }
+
+  /**
+   * Insert the formated (hh:mm:ss.ms) string of the current elapsed time in the
+   * right end of the terminal
+   * @method logElapsedTime
+   * @param {string} [color = 'blue'] The 'chalk' color to be displayed
+   * @example
+   * [WORKING] 5 links crawled                                   [00:00:03.071]
+   */
+  log(color = 'blue') {
+    const width = process.stdout.columns - 3;
+    const elapsed = this.formatedElapsedTime();
+    const position = width - elapsed.length;
+    process.stdout.cursorTo(position);
+    process.stdout.write(chalk[color](`[${elapsed}]`));
+  }
+
+}
+
+module.exports = Elapsy;
